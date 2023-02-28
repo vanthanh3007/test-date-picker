5d15a60e96d600bedf4a8dd38572fb5b001c5ebc
DESKTOP-3QNTRRQ\ALLIANCEITSC <vanthanh300798@gmail.com>
2023-02-28 10:35:31 +0700

    commit main

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import {Component} from "./Component/Modal"
+import { Button } from 'react-native'
+import DatePicker from 'react-native-date-picker'
+
 
 export default function App() {
+  const [date, setDate] = useState(new Date())
+  const [open, setOpen] = useState(false)
+  console.log("date",date)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! 123456</Text>
       <StatusBar style="auto" />
+      {/* <Component /> */}
+      <Button title="Open" onPress={() => setOpen(true)} />
+      <DatePicker
+        modal
+        open={open}
+        date={date}
+        onConfirm={(date) => {
+          setOpen(false)
+          setDate(date)
+        }}
+        onCancel={() => {
+          setOpen(false)
+        }}
+      />
     </View>
   );
 }
